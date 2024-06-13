0784c09df2ed27f8919dd75161c928685e80b7bb
beva3 <mizabienvenu@gmail.com>
2024-06-13 13:23:13 +0300

    on a reussit de chercher l'image vers mon compte unsplash

--- searchImage/script.js
+++ searchImage/script.js
@@ -8,7 +8,46 @@ const showMore = document.getElementById('show-more-button');
 let inputData = "";
 let page = 1;
 
-function searchImages(){
+async function searchImages(){
+    //! what is async
     inputData = inputEl.value;
-    
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const response = await fetch(url); //! do ?
+    const data = await response.json(); //! do ?
+
+    const results = data.results;
+
+    if(page ===1){
+        searchResults.innerHTML = "";
+    }
+
+    results.map((result) =>{
+        const imageWrapper = document.createElement("div");//!wrapp
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+    page++;
+    if(page >1){
+        showMore.style.display = "block";
+    }
 }
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    page =1;
+    searchImages();
+});
+
+showMore.addEventListener('click', () => {
+    searchImages();
+});
