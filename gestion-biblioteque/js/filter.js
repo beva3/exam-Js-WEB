950c92e81589e905ee1cec437a77415d8c43a8b9
beva3 <mizabienvenu@gmail.com>
2024-06-19 00:25:29 +0300

    modif

--- gestion-biblioteque/js/filter.js
+++ gestion-biblioteque/js/filter.js
@@ -1,12 +1,17 @@
-let filter = document.getElementById('find').value.toUpperCase();
-let items = document.querySelectorAll('.product');
-let l = document.getElementsByTagName('h3');
+
 
 // find book
 function search(){
+    let filter = document.getElementById('find').value.toUpperCase();
+    let items = document.querySelectorAll('.product');
+    let l = document.getElementsByTagName('h3');
+
     // alert('search');
-    for(let i ; i<l.length ; i++){
+    // print book
+    // console.log(filter);
+    for(let i=0 ; i<l.length ; i++){
         let a = items[i].getElementsByTagName('h3')[0];
+        console.log('a = ',a);
         let value=a.innerHTML || a.innerTEXT || a.textContent;
 
         if(value.toUpperCase().indexOf(filter) >-1){
